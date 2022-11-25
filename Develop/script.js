8d9120e2e7643065d8123433d562fb4efd3b119b
Lance_Lake <temp@MostDeadlyGame.com>
2022-11-25 14:18:03 -0800

    Completed

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,14 @@
-/*
-
-
-WHEN I click into a timeblock
-THEN I can enter an event
-
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-
-WHEN I refresh the page
-THEN the saved events persist
-
-https://day.js.org/docs/en/get-set/get-set
-
-*/
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
-
-
 $(function () {
 
 	var now = dayjs();
 	var longDate = now.$d;
 	var hour = dayjs().hour();
-                             
 	var currentDay = document.getElementById('currentDay');
 	currentDay.textContent = "Today is " + longDate;
 
 	for(i = 8;i < 18; i++) 
 	{
 		var rowId = document.getElementById('hour-' + i);
-		var noteId = document.getElementById('text-' + i);
-		var noteData = document.getElementById('text-' + i).value;
 		var saveButton = document.getElementById('button-' + i);
 
 		var newClass = "future"
@@ -45,19 +18,17 @@ $(function () {
 
 		rowId.classList.add(newClass);
 
-		noteId.textContent = localStorage.getItem('data-' + i);
-
-		saveButton.addEventListener("click", function() 
+		(function(index) 
 		{
-//			console.log(document.getElementById('text-' + i).value);
-			console.log(i,noteData);
-			saveData(i,noteData);
-		});
+			var noteData = document.getElementById('text-' + index);
+			noteData.value = localStorage.getItem('data-' + index);
+
+			saveButton.addEventListener("click", function() 
+			{
+				localStorage.setItem('data-' + index, noteData.value);
+				return;
+			});
+		}
+		)(i);
 	}
 });
-
-function saveData(i, note)
-	{
-		localStorage.setItem('data-' + i, note);
-		return;
-	};
